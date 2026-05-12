4dae65660a1550bbd6d660f7c3ab9fcd91dbc188
Zaki LB <zakarialbouhmadi@gmail.com>
2026-05-12 05:19:47 +0200

    Obscure database credentials in config.php
    Updated database credentials to use placeholders for security.

--- api_files/config.php
+++ api_files/config.php
@@ -1,9 +1,9 @@
 <?php
 // No whitespace before opening PHP tag
-define('DB_HOST', 'sql193.lh.pl');
-define('DB_NAME', 'serwer440925_blog');
-define('DB_USER', 'serwer440925_blog');
-define('DB_PASS', 'greengl@j@OU1!');
+define('DB_HOST', '*************');
+define('DB_NAME', '*************');
+define('DB_USER', '*************');
+define('DB_PASS', '*************');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
   $conn->set_charset('utf8mb4');
